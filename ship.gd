fa93aa0b28bf38aee93cc0fcc2fbe8de83ce0d59
Drahoš <lechaosx@gmail.com>
2025-03-07 20:03:35 +0100

    Update ship movement logic

--- ship.gd
+++ ship.gd
@@ -1,16 +1,32 @@
-class_name Ship extends CharacterBody2D
+class_name PlayerShip extends CharacterBody2D
 
-@export var max_speed = 10000
+@export var steering = 25
+@export var power = 150
+@export var friction = 20
 @export var drag = 0.1
-@export var turn_speed = 2
+@export var traction = 100
+@export var brakes = 100
+@export var ship_length = 50
 
-func _physics_process(delta):
-	var speed = Input.get_axis("ui_down", "ui_up") * max_speed * delta
-
-	if speed != 0:
-		rotation += Input.get_axis("ui_left", "ui_right") * turn_speed * delta
-		velocity = Vector2(speed, 0).rotated(rotation)
-	else:
-		velocity = velocity * (1 - drag)
-
+func _physics_process(delta: float) -> void:
+	var direction = transform.x
+	
+	var acceleration = direction * power * Input.get_action_strength("ui_up")
+	
+	acceleration -= velocity * brakes * Input.get_action_strength("ui_down") * delta
+	
+	acceleration -= velocity * friction * delta
+	acceleration -= velocity * velocity.length() * drag * delta
+	
+	var steer_direction = Input.get_axis("ui_left", "ui_right") * deg_to_rad(steering)
+	
+	var front = +direction * ship_length / 2.0 + velocity * delta
+	var back  = -direction * ship_length / 2.0 - velocity.rotated(steer_direction) * delta
+	
+	var new_heading = back.direction_to(front)
+	
+	velocity = lerp(velocity, new_heading * velocity.length(), traction * delta) + acceleration * delta
+	
+	rotation = new_heading.angle()
+	
 	move_and_slide()
