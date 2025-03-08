2fcf9dc4cd8b82f080dc0c9c35287f453d52ed75
janm2 <jan.mrazek@chyronhego.com>
2025-03-08 13:48:43 +0100

    add cannon cooldown

--- src/projectiles/cannon_ball/cannon_ball.gd
+++ src/projectiles/cannon_ball/cannon_ball.gd
@@ -5,7 +5,7 @@ extends RigidBody2D
 const delay_die_time = 0.3
 var delay_die : float
 
-const speed_limit = 200
+const speed_limit = 300
 const size_vec_scalor = 0.002
 const size_time_scalor = 3
 const time_speed_scalor = 2
@@ -28,5 +28,5 @@ func _physics_process(delta: float) -> void:
 func _on_body_entered(body: Node) -> void:
 	if body is Ship:
 		body.receive_damage(damage)
-		set_collision_mask_value(4, false)
+		set_collision_layer_value(4, false)
 		queue_free()
--- src/weapons/cannon/cannon.gd
+++ src/weapons/cannon/cannon.gd
@@ -5,10 +5,16 @@ var cannon_ball = preload("res://src/projectiles/cannon_ball/cannon_ball.tscn")
 var loc_diff : Vector2
 
 @export var ball_speed = 40000
+@export var cannon_heat_max : int = 5
+@export var cannon_cooldown_time : float = 0.5
+
+var cannon_heat : int 
+var cannon_cooldown : float 
 
 func _input(event):
 	pass
-	if event.is_action_pressed("Click"):
+	if event.is_action_pressed("Click") && cannon_heat <= cannon_heat_max:
+		cannon_heat += 1
 		var instance = cannon_ball.instantiate()
 		instance.apply_force(loc_diff.normalized() * ball_speed + get_parent().velocity)
 		instance.transform = get_parent().transform.translated(loc_diff.normalized() * $Sprite2D.position.x)
@@ -22,4 +28,12 @@ func _process(delta: float) -> void:
 	# set Rotation
 	loc_diff = mouse_pos - local_pos.origin 
 	rotation = loc_diff.angle() - get_parent().rotation
-		
+	
+	cannon_cooldown += delta
+	if cannon_cooldown >= cannon_cooldown_time:
+		cannon_cooldown = 0
+		if cannon_heat > 0:
+			cannon_heat -= 1
+	
+	var color_change =  1.0 - (float(cannon_heat) / float(cannon_heat_max)) * 0.5
+	$Sprite2D.modulate = Color( 1, color_change, color_change)
