c3e582ecad28dfdf9202738ff4253367754da8fd
3nda <jan@smid.sk>
2025-03-08 20:15:51 +0100

    add barrel/explosion

---
+++ src/barrel/barrel_explosion.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+func explosion(damage: int) -> void:
+	for entry1 in get_overlapping_bodies():
+		for entry2 in entry1.get_children():
+			if entry2 is HitpointBar:
+				entry2.receive_damage(damage, HitpointBar.DamageType.EXPLOSION)
