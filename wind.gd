7fc59629be189e06b19f501ed7237e04915b8cea
Eduard Frlicka <edo.frlicka@gmail.com>
2025-03-08 09:51:34 +0100

    directional wind interaction with CharacterBody2D

--- wind.gd
+++ wind.gd
@@ -4,21 +4,18 @@ extends Node
 @export var max_speed: float
 @export var min_speed: float
 
-signal randomize_wind
-signal set_wind(speed:float, direction:Vector2)
-
 var _rng = RandomNumberGenerator.new()
 var _speed: float
 var _direction: Vector2
 
 func _ready() -> void:
-	_on_wind_set_wind(50, Vector2(1,1))
+	set_wind(50, Vector2(1,1))
 
-func _on_wind_randomize_wind() -> void:
+func randomize_wind() -> void:
 	_speed = _rng.randf_range(min_speed, max_speed)
 	_direction = Vector2(1,0).rotated(_rng.randf_range(0, PI*2))
 
-func _on_wind_set_wind(speed: float, direction: Vector2) -> void:
+func set_wind(speed: float, direction: Vector2) -> void:
 	_speed = speed
 	_direction = direction.normalized()
 
@@ -34,7 +31,7 @@ func _physics_process(delta: float) -> void:
 		
 		if affected_object is CharacterBody2D:
 			var save_velocity = affected_object.velocity
-			affected_object.velocity = _direction * _speed * delta * 60
+			affected_object.velocity = save_velocity.normalized() * _direction.rotated(-save_velocity.angle()).x * _speed * delta * 60
 			affected_object.move_and_slide()
 			affected_object.velocity = save_velocity
 			pass
