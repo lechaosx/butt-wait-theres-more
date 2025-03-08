d4932a1a19b330e3da48cb8302bc27325fb9fa09
3nda <jan@smid.sk>
2025-03-08 13:04:15 +0100

    fix fire && add boom1

--- barrel.gd
+++ barrel.gd
@@ -1,10 +1,16 @@
 class_name Barrel
 extends RigidBody2D
 
+# TODO: po impactu se nehybe
+# TODO: ma damage
+# TODO: rozmistit plaminky podle damage
+# TODO: dodelat explosion
+
 var velocity: Vector2 = Vector2(0, 0)
 var fire = preload("res://src/effects/fire/fire.tscn")
 
 func _ready() -> void:
+	#explode()
 	pass
 
 func _physics_process(delta: float) -> void:
@@ -18,18 +24,21 @@ func explode() -> void:
 	var fire1: Fire = fire.instantiate()
 	var fire2: Fire = fire.instantiate()
 	var fire3: Fire = fire.instantiate()
+	var boom1: Fire = fire.instantiate()
 	#TODO: random fire position
-	#fire1.position = position + Vector2(55, 55)
-	#fire2.position = position + Vector2(77, 55)
-	#fire3.position = position + Vector2(99, 55)
+	fire1.position = position + Vector2(55, 55)
+	fire2.position = position + Vector2(77, 55)
+	fire3.position = position + Vector2(99, 55)
+	boom1.position = position + Vector2(111, 55)
 	parent.add_child(fire1)
 	parent.add_child(fire2)
 	parent.add_child(fire3)
+	parent.add_child(boom1)
 
 	var badaboom = func():
 		SignalBus.BarrelExplodeToShip.emit(self)
 
-	fire1.start("fire1", 3, fire1.empty_callback, 0)
-	fire2.start("fire1", 1, fire1.empty_callback, 0.5)
-	fire3.start("fire1", 1.5, badaboom, 0.75)
-	# BOOM
+	fire1.start("fire1", 3)
+	fire2.start("fire1", 1, 0.5)
+	fire3.start("fire1", 1.5, 0.75)
+	boom1.start("boom1", 0.5, 3.5, badaboom)
