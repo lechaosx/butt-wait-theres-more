04ebff4c0791bf211ffbfed207eca3214a190e2a
Drahoš <lechaosx@gmail.com>
2025-03-08 11:45:25 +0100

    Add camera zoom

--- sea.gd
+++ sea.gd
@@ -13,9 +13,9 @@ func create_barrel(pos: Vector2) -> void:
 	#bar.BarrelExplodeToShip.connect()
 	parent.add_child(bar)
 	
-func get_random_point_on_circle(radius: float) -> Vector2:
+func random_point_on_circle(radius: float) -> Vector2:
 	var angle = randf() * TAU
-	return get_viewport_rect().get_center() + Vector2(cos(angle), sin(angle)) * radius
+	return Vector2(cos(angle), sin(angle)) * radius
 
 func count_objects_in_radius(center: Vector2, radius: float, group_name: String) -> int:
 	var count = 0
@@ -35,15 +35,18 @@ func _on_enemy_spawn_timer_timeout() -> void:
 	ship.controller.target = %PlayerShip
 	ship.add_child(ship.controller)
 	
-	ship.position = %PlayerShip.position + get_random_point_on_circle(get_viewport_rect().size.length() / 2)
+	ship.position = %PlayerShip.position + random_point_on_circle(get_viewport().get_visible_rect().size.length() / 2 * 1.5)
 	
 	ship.texture = load("res://assets/Ships/ship (2).png")
 	add_child(ship)
 
 
 func _on_barrel_spawn_timer_timeout() -> void:
-	var screen_radius = get_viewport_rect().size.length() / 2
-	var num_floaters = count_objects_in_radius(%PlayerShip.position, screen_radius * 3, "floaters") 
+	var screen_radius = get_viewport().get_visible_rect().size.length() / 2
+	
+	var max_radius = screen_radius * 3
+	
+	var num_floaters = count_objects_in_radius(%PlayerShip.position, max_radius, "floaters") 
 	if num_floaters < 10:
-		var radius = randf_range(screen_radius * 2, screen_radius * 3)
-		create_barrel(%PlayerShip.position + get_random_point_on_circle(radius))
+		var radius = randf_range(screen_radius * 1.5, max_radius)
+		create_barrel(%PlayerShip.position + random_point_on_circle(radius))
