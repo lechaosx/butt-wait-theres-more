321b5fe666a93e11f63fc22303f0d43106e37ed1
janm2 <jan.mrazek@chyronhego.com>
2025-03-08 17:29:34 +0100

    added impact effect for cannon ball

--- src/projectiles/cannon_ball/cannon_ball.gd
+++ src/projectiles/cannon_ball/cannon_ball.gd
@@ -12,6 +12,7 @@ const size_time_scalor = 3
 const time_speed_scalor = 2
 
 var splash = preload("res://src/effects/splash/splash.tscn")
+var impact = preload("res://src/effects/impact/impact.tscn")
 
 func _physics_process(delta: float) -> void:
 	var new_scale = clamp(linear_velocity.length() * size_vec_scalor + clamp(delay_die, 0, delay_die_time) * size_time_scalor, 1.0, 5.0)
@@ -30,4 +31,7 @@ func _on_body_entered(body: Node) -> void:
 		if child is HitpointBar:
 			child.receive_damage(1)
 	
+	var instance = impact.instantiate()
+	instance.transform = transform
+	get_tree().root.add_child(instance);
 	queue_free()
