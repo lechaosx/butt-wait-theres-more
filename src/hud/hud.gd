b72f6e738925734203ac34c76667add63cd4a085
Eduard Frlicka <edo.frlicka@gmail.com>
2025-03-10 14:26:46 +0100

    player damage popups fixed

--- src/hud/hud.gd
+++ src/hud/hud.gd
@@ -6,7 +6,6 @@ func _ready() -> void:
 	var player_hp_bar : HitpointBar = %PlayerShip/HitpointBar
 	player_hp_bar.hitpoint_update.connect(self.update_hitpoints)
 	player_hp_bar.max_hitpoints_update.connect(self.update_max_hitpoints)
-	player_hp_bar.visible=false
 	update_max_hitpoints(player_hp_bar.max_hitpoints)
 	update_hitpoints(player_hp_bar.hitpoints)
 	pass # Replace with function body.
