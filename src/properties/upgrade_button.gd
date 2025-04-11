7d6a967e970443e5fa5a5def91edb98daabd28ee
Drahomír Dlabaja <lechaosx@gmail.com>
2025-04-11 12:53:36 +0200

    Fix broken font colors

--- src/properties/upgrade_button.gd
+++ src/properties/upgrade_button.gd
@@ -2,6 +2,9 @@ class_name UpgradeButton extends HBoxContainer
 
 signal button_pressed
 
+@export var no_money_font_color = Color(0.53, 0.141333, 0.106)
+@export var no_money_font_color_hover = Color(0.96, 0.4232, 0.3744)
+
 var upgrade: Upgrade
 var balance: int = 0
 
@@ -18,8 +21,8 @@ func _process(_delta: float) -> void:
 		$Button.text = str(upgrade.price())
 		
 	if balance < upgrade.price():
-		$Button.add_theme_color_override("font_color", Color("#93413aff"))
-		$Button.add_theme_color_override("font_hover_color", Color("#93413aff"))
+		$Button.add_theme_color_override("font_color", no_money_font_color)
+		$Button.add_theme_color_override("font_hover_color", no_money_font_color_hover)
 	else:
 		$Button.remove_theme_color_override("font_color")
 		$Button.remove_theme_color_override("font_hover_color")
