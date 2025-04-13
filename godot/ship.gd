ae81d3c9503003b272afb9db65532e6a5a7f7924
Drahomír Dlabaja <lechaosx@gmail.com>
2025-04-13 15:07:50 +0200

    Static typing

--- godot/ship.gd
+++ godot/ship.gd
@@ -73,7 +73,7 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
+		var collision := get_slide_collision(i)
 		if collision.get_collider() is RigidBody2D:
 			collision.get_collider().apply_central_impulse(-collision.get_normal() * 50)
 
