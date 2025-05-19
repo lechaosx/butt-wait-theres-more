b1e7525ed5301d5fd11d780f0292aab6d45243a8
Drahomír Dlabaja <lechaosx@gmail.com>
2025-05-19 16:54:25 +0200

    Further unify names

--- godot/abilities/barel_dropping.gd
+++ godot/abilities/barel_dropping.gd
@@ -1,7 +1,7 @@
 @tool extends Node
 
-@export var sea: Sea
-@export var body: CharacterBody2D
+@export var world: Node
+@export var parent: CharacterBody2D
 
 @export var interval: float:
 	set(value):
@@ -20,13 +20,13 @@ func _on_timer_timeout() -> void:
 		
 	var instance := preload("res://projectiles/barrel_ball/barrel_ball.tscn").instantiate()
 
-	var pos := body.global_position + Vector2(-100, 0).rotated(body.global_rotation)
-	var dir := -1 * body.global_transform.x
+	var pos := parent.global_position + Vector2(-100, 0).rotated(parent.global_rotation)
+	var dir := -1 * parent.global_transform.x
 	var speed := 4000 #hardcoded:40000
 	var offset := 20 #hardcoded:20
 
-	instance.position = pos + body.global_transform.x * offset
-	instance.add_collision_exception_with(body)
-	instance.apply_force(speed * dir + body.velocity)
+	instance.position = pos + parent.global_transform.x * offset
+	instance.add_collision_exception_with(parent)
+	instance.apply_force(speed * dir + parent.velocity)
 
-	sea.add_child(instance)
+	world.add_child(instance)
