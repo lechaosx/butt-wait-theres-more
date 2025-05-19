9454746a76c80c6b420b330f482a09cf7e5aba05
Drahomír Dlabaja <lechaosx@gmail.com>
2025-05-19 15:18:09 +0200

    Do not use resource for ship properties

--- godot/new_player/ship_movement_component.gd
+++ godot/new_player/ship_movement_component.gd
@@ -1,7 +1,14 @@
 class_name ShipMovementComponent extends Node
 
 @export var character: CharacterBody2D
-@export var properties: ShipProperties
+
+@export var steering_angle: float = 25
+@export var power: float = 100
+@export var friction: float = 30
+@export var drag: float = 0.1
+@export var traction: float = 0.8
+@export var brakes: float = 100
+@export var ship_length: float = 50
 
 var acceleration_intent: float = 0.0
 var brake_intent: float = 0.0
@@ -10,20 +17,20 @@ var steer_intent: float = 0.0
 func _physics_process(delta: float) -> void:
 	var direction := character.transform.y
 
-	var acceleration := direction * properties.power * acceleration_intent
+	var acceleration := direction * power * acceleration_intent
 
-	acceleration -= character.velocity * properties.brakes * brake_intent * delta
-	acceleration -= character.velocity * properties.friction * delta
-	acceleration -= character.velocity * character.velocity.length() * properties.drag * delta
+	acceleration -= character.velocity * brakes * brake_intent * delta
+	acceleration -= character.velocity * friction * delta
+	acceleration -= character.velocity * character.velocity.length() * drag * delta
 
-	var steer_direction := steer_intent * deg_to_rad(properties.steering_angle)
+	var steer_direction := steer_intent * deg_to_rad(steering_angle)
 
-	var front := +direction * properties.ship_length / 2.0 + character.velocity * delta
-	var back  := -direction * properties.ship_length / 2.0 - character.velocity.rotated(steer_direction) * delta
+	var front := +direction * ship_length / 2.0 + character.velocity * delta
+	var back  := -direction * ship_length / 2.0 - character.velocity.rotated(steer_direction) * delta
 
 	var new_heading := back.direction_to(front)
 
-	character.velocity = lerp(character.velocity, new_heading * character.velocity.length(), properties.traction) + acceleration * delta
+	character.velocity = lerp(character.velocity, new_heading * character.velocity.length(), traction) + acceleration * delta
 
 	character.rotation = new_heading.rotated(deg_to_rad(-90)).angle()
 
--- godot/new_player/ship_properties.gd
+++
@@ -1,9 +0,0 @@
-class_name ShipProperties extends Resource
-
-@export var steering_angle: float
-@export var power: float
-@export var friction: float
-@export var drag: float
-@export var traction: float
-@export var brakes: float
-@export var ship_length: float
