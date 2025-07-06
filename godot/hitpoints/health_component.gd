5320386731c161f10b335db24a7f62162253cd78
Drahomír Dlabaja <lechaosx@gmail.com>
2025-07-06 21:22:02 +0200

    Fix bug with health bar not working correctly with no save

--- godot/hitpoints/health_component.gd
+++ godot/hitpoints/health_component.gd
@@ -5,22 +5,19 @@ signal health_updated
 
 @export var max_health: int = 5:
 	set(value):
-		if value != max_health:
-			max_health = value
-			max_health_updated.emit()
-			health = clamp(health, 0, max_health)
+		max_health = value
+		max_health_updated.emit()
+		health = clamp(health, 0, max_health)
 
 @export var health: int = 5:
 	set(value):
 		value = clamp(value, 0, max_health)
-		if value != health:
+		var damage := health - value
+		if damage > 0:
+			_spawn_popup(damage)
 			
-			var damage := health - value
-			if damage > 0:
-				_spawn_popup(damage)
-				
-			health = value
-			health_updated.emit()
+		health = value
+		health_updated.emit()
 
 func _spawn_popup(value: int) -> void:
 	var popup := preload("res://hitpoints/damage_popup.tscn").instantiate()
